f540d6e7a0193af728fafc3d1713ac9f566df718
SasiSk06 <bgmsasi@gmail.com>
2026-08-22 13:28:26 +0530

    Force refresh latest process arrow styles

--- script.js
+++ script.js
@@ -225,7 +225,7 @@ if (footerText) {
     const link = document.createElement("link");
     link.id = "processPremiumStyles";
     link.rel = "stylesheet";
-    link.href = "process-premium.css";
+    link.href = "process-premium.css?v=20260822-arrows-v2";
     document.head.appendChild(link);
   }
 
